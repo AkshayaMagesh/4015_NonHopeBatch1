cec7f2eb5a24cd421218db288c77f28ac388f022
Akshaya Magesh <akshayamagesh0206@gmail.com>
2026-04-08 08:10:04 +0530

    Add files via upload

---
+++ day_14/HashMapImplementation.java
@@ -0,0 +1,10 @@
+package day_14;
+
+import java.util.*;
+public class HashMapImplementation {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        System.out.println(map.get("hello"));;
+    }
+}
